99b143466ed5e7bb495da33b8af5052ee51188a3
Thomas Legrand <pro.legrand.thomas@gmail.com>
2024-06-30 00:54:21 +0200

    fix

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -15,13 +15,7 @@ const Home = () => {
 
   const fetchDogs = async () => {
     try {
-      const response = await fetch('http://localhost:3001/api/dogs', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
-        },
-      });
+      const response = await fetch('http://localhost:3001/api/dogs');
       if (!response.ok) {
         throw new Error('Failed to fetch dogs');
       }
